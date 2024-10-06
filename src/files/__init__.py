e314a98954867d0c9e6db982d7257ec3da8d31aa
Ismael Varela <info@varelait.com>
2024-10-06 19:19:39 -0400

    file function to write the documentation

--- src/files/__init__.py
+++ src/files/__init__.py
@@ -1,17 +1,48 @@
 
 section = """
-## Files:
+## Files
 
-Open file and return a corresponding _file object_. If the file cannot be opened, an OSError is raised.
+In Python you can obtain the instance of a `file` by using the `open` built-in function.
 
-file is a path-like object giving the pathname (absolute or relative to the current working directory) 
+File is a path-like object giving the pathname (absolute or relative to the current working directory) 
 of the file to be opened or an integer file descriptor of the file to be wrapped. 
 (If a file descriptor is given, it is closed when the returned I/O object is closed unless closefd is set to False.).
+
+### `open` function
+Open file and return a corresponding _file object_. If the file cannot be opened, an OSError is raised.
+ 
+**Open Syntax**
+```
+open(file, mode='r', buffering=-1, encoding=None, errors=None, newline=None, closefd=True, opener=None)
+```
+
+Mode is an optional string that specifies the mode in which the file is opened. It defaults to 'r' which means open for reading in text mode.
+
+|Char|Meaning|
+|----|-------|
+|'r'|open for reading (default)|
+|'w'| open for writing, truncating the file first|
+|'x'| open for exclusive creation, failing if the file already exists|
+|'a'| open for writing, appending to the end of file if it exists|
+|'b'| binary mode|
+|'t'| text mode (default)|
+|'+'| open for updating (reading and writing)|
+
+Example of the code that writes this document:
+```python
+try:
+    file = open("README.md", "w")
+    file.write(content)
+    print("File writed.")
+except:
+    print("Error: can't read or write file.")
+```
 """
-def workingWithFiles():
-    print(section)
+
+def writeToFile(content: str):
     try:
-        file = open("README.md", "a")
-        print(file)
+        file = open("README.md", "w")
+        file.write(content)
+        print("File writed.")
     except:
-        print("Error: can't read file.")
+        print("Error: can't read or write file.")
---
+++ src/strings/stringDoc.py
@@ -0,0 +1,49 @@
+
+section = """
+## String
+
+A string is a sequence of characters.
+```python
+oneString = "hello" #>>> hello
+anotherString = "world!" #>>> world!
+```
+
+The contatenate operator it the `+` symbol.
+```python
+gretting = oneString + " " + anotherString #>>> hello world!
+```
+
+The `len` function take a string argument and returns interger representing the length of a string.
+```python
+len(gretting) #>>> 12
+```
+
+Looping through strings
+```python
+index = 0
+while index < len(greetting):
+    letter = fruit[index]
+    index = index + 1
+    print(letter) 
+
+for letter in greetting:
+    print(letter)
+
+#both loops will print: 
+#>> h
+#>> e
+#>> l
+#>> l
+#>> o
+#>> 
+#>> w
+#>> o
+#>> r
+#>> l
+#>> d
+#>> !
+```
+"""
+
+def stringSection() -> str:
+    return ""
